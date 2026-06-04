71e7033b9483f06da06a7c76b97e7d4eb5a41f6b
Aakash Shrestha <aakash.cerestha90@gmail.com>
2026-06-04 22:05:01 +0545

    Refactor CORS configuration to allow all origins and update nodemailer transport settings for secure connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,13 +18,7 @@ const allowedOrigins = ["http://localhost:5173", process.env.CLIENT_URL];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "*",
     credentials: true,
   }),
 );
--- backend/src/mail/nodemailer/nodemailer.config.js
+++ backend/src/mail/nodemailer/nodemailer.config.js
@@ -5,15 +5,15 @@ dotenv.config({ quiet: true });
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  family: 4, // Use IPv4
-  connectionTimeout: 15000,
-  socketTimeout: 15000,
+  // family: 4, // Use IPv4
+  // connectionTimeout: 15000,
+  // socketTimeout: 15000,
 });
 
 export const sender = process.env.EMAIL_USER;
