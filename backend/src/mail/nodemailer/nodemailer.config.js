1d09aee5778ffa7c33a7064d7dcb39ed902a183f
Aakash Shrestha <aakash.cerestha90@gmail.com>
2026-06-04 22:18:29 +0545

    Refactor nodemailer configuration to use port 587 and disable secure connection

--- backend/src/mail/nodemailer/nodemailer.config.js
+++ backend/src/mail/nodemailer/nodemailer.config.js
@@ -4,17 +4,28 @@ import dotenv from "dotenv";
 dotenv.config({ quiet: true });
 
 export const transporter = nodemailer.createTransport({
+  // host: "smtp.gmail.com",
+  // service: "gmail",
+  // port: 465,
+  // secure: true,
+  // auth: {
+  //   user: process.env.EMAIL_USER,
+  //   pass: process.env.EMAIL_PASS,
+  // },
   host: "smtp.gmail.com",
-  service: "gmail",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  // family: 4, // Use IPv4
-  // connectionTimeout: 15000,
-  // socketTimeout: 15000,
+
+  family: 4, // Force IPv4
+
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 export const sender = process.env.EMAIL_USER;
