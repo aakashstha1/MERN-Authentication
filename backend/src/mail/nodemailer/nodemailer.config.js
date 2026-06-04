fdbe4b1da626e3daf21b9fef8934b3ba3f6263a6
Aakash Shrestha <aakash.cerestha90@gmail.com>
2026-06-04 22:27:45 +0545

    Add DNS configuration to enforce IPv4 and clean up nodemailer transport settings

--- backend/src/mail/nodemailer/nodemailer.config.js
+++ backend/src/mail/nodemailer/nodemailer.config.js
@@ -1,5 +1,7 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
 
 dotenv.config({ quiet: true });
 
@@ -20,8 +22,6 @@ export const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-
-  family: 4, // Force IPv4
 
   connectionTimeout: 30000,
   greetingTimeout: 30000,
